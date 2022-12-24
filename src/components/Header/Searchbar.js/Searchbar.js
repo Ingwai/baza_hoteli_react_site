e4e70f5f0740055551ae468b7c84f91b9dd1c201
Ingwai <ingwai@wp.pl>
2022-12-24 21:17:28 +0100

    initial react app

---
+++ src/components/Header/Searchbar.js/Searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Searchbar = () => {
+	return (
+		<div>
+			<input style={{ width: 'calc(100% - 20px)' }} className="form-control" type='text' placeholder='Szukaj...' />
+			<button className='btn btn-primary'>Szukaj</button>
+		</div>
+	);
+};
+
+export default Searchbar;
